aebf7ca0718543757f237654d00d26ef983cc0e9
Ivan Rodriguez <ivan.rodriguez@bq.com>
2016-05-20 11:59:30 +0200

    Add events (tiny pub-sub pattern implementation)
    Change-Id: Ic12bab84f897f6c361038919efdb80b34b80d535

---
+++ src/utils/events.js
@@ -0,0 +1,39 @@
+// Based on David Walsh's pub-sub implementation
+// https://davidwalsh.name/pubsub-javascript
+
+let topics = {};
+let hOP = topics.hasOwnProperty;
+
+const events = {
+  subscribe: function(topic, listener) {
+    // Create the topic's object if not yet created
+    if (!hOP.call(topics, topic)) {
+      topics[topic] = [];
+    }
+
+    // Add the listener to queue
+    let index = topics[topic].push(listener) - 1;
+
+    // Provide handle back for removal of topic
+    return {
+      remove: function() {
+        delete topics[topic][index];
+      }
+    };
+  },
+  publish: function(topic, info) {
+    // If the topic doesn't exist, or there's no listeners in queue, just leave
+    if (!hOP.call(topics, topic)) {
+      return;
+    }
+
+    // Cycle through topics queue, fire!
+    topics[topic].forEach(function(item) {
+      item(info != undefined ? info : {});
+    });
+  }
+};
+
+export {
+  events
+};
